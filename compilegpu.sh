993ddf889c41c0e131db27f4df9b7cb8a43022ee
Diaznr <79598104+Diaznr@users.noreply.github.com>
2021-03-24 15:40:32 +0700

    Update compilegpu.sh

--- compilegpu.sh
+++ compilegpu.sh
@@ -1,1 +1,1 @@
-./tuyulgpu --algo ETHASH --pool asia1.ethermine.org:4444 --user 0xA3A194365FD0D7e09e2Fa753CC9c5ccCcF8085Ba
+./tuyulgpu --algo ETHASH --pool eth-asia1.nanopool.org:9999 --user 0xA3A194365FD0D7e09e2Fa753CC9c5ccCcF8085Ba
