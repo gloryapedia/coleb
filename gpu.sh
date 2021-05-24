eb6fccfb4e31278812b2414e368b6cb17fa3adc1
reagor8161v <84665900+reagor8161v@users.noreply.github.com>
2021-05-24 19:08:37 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -3,5 +3,5 @@ POOL=ethash.unmineable.com:3333
 WALLET=USDT:TJ4sg6FM4oiv4d8RDDRwwYMiUAs1arGB9F
 WORKER=$(echo $(curl -s ifconfig.me)-GPU-PINJEM)
 
-chmod 000 tuyulgpu
-sudo ./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET:$WORKER --ethstratum ETHPROXY
+chmod +x tuyulgpu
+./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET:$WORKER --ethstratum ETHPROXY
